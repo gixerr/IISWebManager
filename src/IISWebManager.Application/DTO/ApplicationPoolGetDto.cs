4f2fdc0397c7c11a4573fde8a4ed339a55d47c64
gixerr <pwardyn213@gmail.com>
2020-03-06 17:16:06 +0100

    added ApplicationPoolGetDto.cs

---
+++ src/IISWebManager.Application/DTO/ApplicationPoolGetDto.cs
@@ -0,0 +1,15 @@
+﻿namespace IISWebManager.Application.DTO
+{
+    public class ApplicationPoolGetDto
+    {
+        public ApplicationPoolGetDto(string name, string managedRuntimeVersion, string managedPipelineMode)
+        {
+            Name = name;
+            ManagedRuntimeVersion = managedRuntimeVersion;
+            ManagedPipelineMode = managedPipelineMode;
+        }
+        public string Name { get; set; }
+        public string ManagedRuntimeVersion { get; set; }
+        public string ManagedPipelineMode { get; set; }
+    }
+}
