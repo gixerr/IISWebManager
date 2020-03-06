d041b2c94d17a77189cd3f521bb5cf8c26f8fc63
gixerr <pwardyn213@gmail.com>
2020-03-06 17:16:26 +0100

    added queries

---
+++ src/IISWebManager.Application/Queries/ApplicationPools/BrowseApplicationPools.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using IISWebManager.Application.DTO;
+
+namespace IISWebManager.Application.Queries.ApplicationPools
+{
+    public class BrowseApplicationPools : IQuery<IEnumerable<ApplicationPoolGetDto>>
+    {
+    }
+}
---
+++ src/IISWebManager.Application/Queries/IQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace IISWebManager.Application.Queries
+{
+    public interface IQuery<TResult>
+    {
+    }
+}
