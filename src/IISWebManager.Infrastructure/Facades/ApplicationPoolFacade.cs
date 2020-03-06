a91f5b782bce3780fabae1ea82f7eb722328254c
gixerr <pwardyn213@gmail.com>
2020-03-06 17:24:43 +0100

    added facades

---
+++ src/IISWebManager.Infrastructure/Facades/ApplicationPoolFacade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Web.Administration;
+
+namespace IISWebManager.Infrastructure.Facades
+{
+    public class ApplicationPoolFacade : IApplicationPoolFacade
+    {
+        private readonly ServerManager _serverManager;
+
+        public ApplicationPoolFacade(ServerManager serverManager)
+        {
+            _serverManager = serverManager;
+        }
+
+        public ApplicationPoolCollection BrowseApplicationPools()
+            => _serverManager.ApplicationPools;
+
+        public ApplicationPoolCollection GetApplicationPools(string subString)
+            => (ApplicationPoolCollection) _serverManager.ApplicationPools.Where(x => x.Name.Contains(subString));
+
+        public ApplicationPool GetApplicationPool(string name) =>
+            _serverManager.ApplicationPools
+                .SingleOrDefault(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+        public void StartApplicationPool(ApplicationPool applicationPool)
+            => applicationPool.Start();
+
+        public void StopApplicationPool(ApplicationPool applicationPool) 
+            => applicationPool.Stop();
+    }
+}
---
+++ src/IISWebManager.Infrastructure/Facades/IApplicationPoolFacade.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Web.Administration;
+
+namespace IISWebManager.Infrastructure.Facades
+{
+    public interface IApplicationPoolFacade
+    {
+        ApplicationPoolCollection BrowseApplicationPools();
+        ApplicationPoolCollection GetApplicationPools(string subString);
+        ApplicationPool GetApplicationPool(string name);
+        void StartApplicationPool(ApplicationPool applicationPool);
+        void StopApplicationPool(ApplicationPool applicationPool);
+    }
+}
