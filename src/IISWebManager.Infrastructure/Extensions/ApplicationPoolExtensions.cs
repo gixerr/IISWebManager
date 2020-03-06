63ff98606f4214407ddb0b84da59b1397c143c64
gixerr <pwardyn213@gmail.com>
2020-03-06 17:22:39 +0100

    added ApplicationPoolExtensions.cs

---
+++ src/IISWebManager.Infrastructure/Extensions/ApplicationPoolExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IISWebManager.Application.DTO;
+using Microsoft.Web.Administration;
+
+namespace IISWebManager.Infrastructure.Extensions
+{
+    public static class ApplicationPoolExtensions
+    {
+        public static ApplicationPoolGetDto AsDto(this ApplicationPool applicationPool)
+            => new ApplicationPoolGetDto(applicationPool.Name, applicationPool.ManagedRuntimeVersion,
+                applicationPool.ManagedPipelineMode.ToString());
+
+        public static IEnumerable<ApplicationPoolGetDto> AsDto(this ApplicationPoolCollection applicationPoolCollection)
+            => applicationPoolCollection
+                .Select(x => new ApplicationPoolGetDto(x.Name, x.ManagedRuntimeVersion, 
+                    x.ManagedPipelineMode.ToString()));
+    }
+}
