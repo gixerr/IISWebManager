124d94b26ad495663d420d56fbb35cee4a19840c
gixerr <pwardyn213@gmail.com>
2020-03-08 15:48:18 +0100

    added ApplicationPoolAlreadyStopped.cs

---
+++ src/IISWebManager.Application/Exceptions/ApplicationPoolAlreadyStopped.cs
@@ -0,0 +1,12 @@
+﻿namespace IISWebManager.Application.Exceptions
+{
+    public class ApplicationPoolAlreadyStopped : ApplicationException
+    {
+        public override string Code => "application_pool_already_stopped";
+        
+        public ApplicationPoolAlreadyStopped(string name) : base($"Application pool '{name}' is already stopped.")
+        {
+        }
+
+    }
+}
