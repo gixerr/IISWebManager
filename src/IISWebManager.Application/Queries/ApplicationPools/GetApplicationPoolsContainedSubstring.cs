d3a9f9e0caa6107bc2b8ab698b5cd6f27824501f
gixerr <pwardyn213@gmail.com>
2020-03-08 15:06:45 +0100

    added query GetApplicationPoolsContainedSubstring

---
+++ src/IISWebManager.Application/Queries/ApplicationPools/GetApplicationPoolsContainedSubstring.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using IISWebManager.Application.DTO;
+
+namespace IISWebManager.Application.Queries.ApplicationPools
+{
+    public class GetApplicationPoolsContainedSubstring : IQuery<IEnumerable<ApplicationPoolGetDto>>
+    {
+        public string Substring { get; set; }
+    }
+}
