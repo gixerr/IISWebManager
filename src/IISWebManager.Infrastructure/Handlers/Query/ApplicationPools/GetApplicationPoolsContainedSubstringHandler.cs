22506e70d6d291d93f998627c40df00e5efb24ec
gixerr <pwardyn213@gmail.com>
2020-03-08 15:08:28 +0100

    added GetApplicationPoolsContainedSubstringHandler

---
+++ src/IISWebManager.Infrastructure/Handlers/Query/ApplicationPools/GetApplicationPoolsContainedSubstringHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using IISWebManager.Application.DTO;
+using IISWebManager.Application.Exceptions;
+using IISWebManager.Application.Queries.ApplicationPools;
+using IISWebManager.Infrastructure.Extensions;
+using IISWebManager.Infrastructure.Facades;
+
+namespace IISWebManager.Infrastructure.Handlers.Query.ApplicationPools
+{
+    public class GetApplicationPoolsContainedSubstringHandler : IQueryHandler<GetApplicationPoolsContainedSubstring,
+        IEnumerable<ApplicationPoolGetDto>>
+    {
+        private readonly IApplicationPoolFacade _applicationPoolFacade;
+
+        public GetApplicationPoolsContainedSubstringHandler(IApplicationPoolFacade applicationPoolFacade)
+        {
+            _applicationPoolFacade = applicationPoolFacade;
+        }
+        public IEnumerable<ApplicationPoolGetDto> Handle(GetApplicationPoolsContainedSubstring query)
+        {
+            if (query is null)
+            {
+                throw new MissingQueryException($"Handler '{GetType().Name}' received null query.");
+            }
+            var applicationPools = _applicationPoolFacade.GetApplicationPools(query.Substring);
+
+            return applicationPools.AsDto();
+        }
+    }
+}
