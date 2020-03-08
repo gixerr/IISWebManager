f274689ac1bfae4faccb14b299fcd1645f1f3819
gixerr <pwardyn213@gmail.com>
2020-03-08 15:06:27 +0100

    added query GetApplicationPool

---
+++ src/IISWebManager.Application/Queries/ApplicationPools/GetApplicationPool.cs
@@ -0,0 +1,9 @@
+﻿using IISWebManager.Application.DTO;
+
+namespace IISWebManager.Application.Queries.ApplicationPools
+{
+    public class GetApplicationPool : IQuery<ApplicationPoolGetDto>
+    {
+        public string Name { get; set; }
+    }
+}
