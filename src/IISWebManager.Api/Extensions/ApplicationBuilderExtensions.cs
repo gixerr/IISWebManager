7ad879086942040e19049857dea347f0c98a998b
gixerr <pwardyn213@gmail.com>
2020-03-08 15:02:28 +0100

    added Extensions to Api

---
+++ src/IISWebManager.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using IISWebManager.Infrastructure.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace IISWebManager.Api.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void DisposeAutofacContainer(this IApplicationBuilder app,
+            IHostApplicationLifetime hostApplicationLifetime)
+            => hostApplicationLifetime.ApplicationStopped.Register(() 
+                => app.ApplicationServices.GetAutofacRoot().Dispose());
+
+        public static void UseErrorHandler(this IApplicationBuilder app)
+            => app.UseMiddleware<ErrorHandlerMiddleware>();
+    }
+}
