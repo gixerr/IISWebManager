a93ca5b2133e56048cefdeae5ec0b929050e5e7e
gixerr <pwardyn213@gmail.com>
2020-03-08 15:08:11 +0100

    added GetApplicationPoolHandler

---
+++ src/IISWebManager.Infrastructure/Handlers/Query/ApplicationPools/GetApplicationPoolHandler.cs
@@ -0,0 +1,28 @@
+﻿using IISWebManager.Application.DTO;
+using IISWebManager.Application.Exceptions;
+using IISWebManager.Application.Queries.ApplicationPools;
+using IISWebManager.Infrastructure.Extensions;
+using IISWebManager.Infrastructure.Facades;
+
+namespace IISWebManager.Infrastructure.Handlers.Query.ApplicationPools
+{
+    public class GetApplicationPoolHandler : IQueryHandler<GetApplicationPool, ApplicationPoolGetDto>
+    {
+        private readonly IApplicationPoolFacade _applicationPoolFacade;
+
+        public GetApplicationPoolHandler(IApplicationPoolFacade applicationPoolFacade)
+        {
+            _applicationPoolFacade = applicationPoolFacade;
+        }
+        public ApplicationPoolGetDto Handle(GetApplicationPool query)
+        {
+            if (query is null)
+            {
+                throw new MissingQueryException($"Handler '{GetType().Name}' received null query.");
+            }
+            var applicationPool = _applicationPoolFacade.GetApplicationPool(query.Name);
+
+            return applicationPool.AsDto();
+        }
+    }
+}
