a9fb69e6cc771be7207282dfd87f10f286f6192b
gixerr <pwardyn213@gmail.com>
2020-03-08 15:48:06 +0100

    added ApplicationPoolNotExistsException.cs

---
+++ src/IISWebManager.Application/Exceptions/ApplicationPoolNotExistsException.cs
@@ -0,0 +1,14 @@
+﻿using IISWebManager.Application.Exceptions;
+
+namespace IISWebManager.Infrastructure.Facades
+{
+    public class ApplicationPoolNotExistsException : ApplicationException
+    {
+        public override string Code => "application_pool_not_exist";
+
+        public ApplicationPoolNotExistsException(string name) : base(
+            $"Application pool with provided name '{name}' doesn't exists.")
+        {
+        }
+    }
+}
