66d948a6c0f405df99faaa5b744e00d6215decb7
gixerr <pwardyn213@gmail.com>
2020-03-09 23:49:39 +0100

    cleanup

--- src/IISWebManager.Infrastructure/Facades/ApplicationPoolFacade.cs
+++ src/IISWebManager.Infrastructure/Facades/ApplicationPoolFacade.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using IISWebManager.Application.Exceptions;
 using Microsoft.Web.Administration;
 
 namespace IISWebManager.Infrastructure.Facades
