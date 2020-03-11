0cd8971b5fe840f7d6a111dd208537c5725b91f6
gixerr <pwardyn213@gmail.com>
2020-03-11 22:49:19 +0100

    added QueryExtensions

---
+++ src/IISWebManager.Application/Extensions/QueryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using IISWebManager.Application.Exceptions;
+using IISWebManager.Application.Queries;
+
+namespace IISWebManager.Application.Extensions
+{
+    public static class QueryExtensions
+    {
+        public static void ThrowIfNull<TResult>(this IQuery<TResult> query, string handlerName)
+        {
+            if (query is null) throw new MissingQueryException($"Handler '{handlerName}' received null query.");
+        }
+    }
+}
