b137e8a02904979b32fa3cf19a76a0b0a908c757
gixerr <pwardyn213@gmail.com>
2020-03-12 20:30:57 +0100

    removed unnecessary ext methods

--- src/IISWebManager.Infrastructure/Extensions/ApplicationPoolExtensions.cs
+++ src/IISWebManager.Infrastructure/Extensions/ApplicationPoolExtensions.cs
@@ -1,27 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using IISWebManager.Application.DTO;
-using IISWebManager.Application.Exceptions;
+﻿using IISWebManager.Application.Exceptions;
 using Microsoft.Web.Administration;
 
 namespace IISWebManager.Infrastructure.Extensions
 {
     public static class ApplicationPoolExtensions
     {
-        public static ApplicationPoolGetDto AsDto(this ApplicationPool applicationPool)
-            => new ApplicationPoolGetDto(applicationPool.Name, applicationPool.ManagedRuntimeVersion,
-                applicationPool.ManagedPipelineMode.ToString(), applicationPool.State.ToString());
-
-        public static IEnumerable<ApplicationPoolGetDto> AsDto(this ApplicationPoolCollection applicationPoolCollection)
-            => applicationPoolCollection
-                .Select(x => new ApplicationPoolGetDto(x.Name, x.ManagedRuntimeVersion, 
-                    x.ManagedPipelineMode.ToString(), x.State.ToString()));
-
-        public static IEnumerable<ApplicationPoolGetDto> AsDto(this IEnumerable<ApplicationPool> applicationPools)
-            => applicationPools
-                .Select(x => new ApplicationPoolGetDto(x.Name, x.ManagedRuntimeVersion,
-                    x.ManagedPipelineMode.ToString(), x.State.ToString()));
-
         public static void ThrowIfNull(this ApplicationPool applicationPool, string name)
         {
             if (applicationPool is null) throw new ApplicationPoolNotExistsException(name);
