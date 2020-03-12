a99205d0e23f9537cea3c04a4e9bf5d30ce2e46e
gixerr <pwardyn213@gmail.com>
2020-03-12 20:30:33 +0100

    updated ApplicationPoolHelpers

---
+++ src/IISWebManager.Infrastructure/Helpers/ApplicationPoolHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.Web.Administration;
+
+namespace IISWebManager.Infrastructure.Helpers
+{
+    public static class ApplicationPoolHelpers
+    {
+        public static int GetNumberOfApplicationPoolApplications(string applicationPoolName)
+        {
+            using var serverManager = new ServerManager();
+            
+            return serverManager.Sites
+                .SelectMany(s => s.Applications
+                    .Where(a => a.ApplicationPoolName.Equals(applicationPoolName)))
+                .Count();
+        }
+    }
+}
