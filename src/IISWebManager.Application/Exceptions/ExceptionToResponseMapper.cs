f2480c82239da1f7afdbab395543e440cd6eae22
gixerr <pwardyn213@gmail.com>
2020-03-12 13:13:42 +0100

    added UnauthorizedAccessException mapping

--- src/IISWebManager.Application/Exceptions/ExceptionToResponseMapper.cs
+++ src/IISWebManager.Application/Exceptions/ExceptionToResponseMapper.cs
@@ -13,6 +13,12 @@ namespace IISWebManager.Application.Exceptions
                     HttpStatusCode.BadRequest),
                 ApplicationException ex => new ExceptionResponse(new {code = ex.Code, message = ex.Message},
                     HttpStatusCode.BadRequest),
+                UnauthorizedAccessException ex => new ExceptionResponse(
+                    new
+                    {
+                        code = "unauthorized_access",
+                        message = "Cannot read configuration file due to insufficient permissions. Forbidden access to Server Manager."
+                    }, HttpStatusCode.Unauthorized),
                 _ => new ExceptionResponse(new {code = "Error", message = "Something went wrong."},
                     HttpStatusCode.BadRequest)
             };
