32fd99aac0325858333f65e9de86b4e11748fe20
gixerr <pwardyn213@gmail.com>
2020-03-13 15:25:29 +0100

    added CommandExtensions

---
+++ src/IISWebManager.Application/Extensions/CommandExtensions.cs
@@ -0,0 +1,13 @@
+﻿using IISWebManager.Application.Commands;
+using IISWebManager.Application.Exceptions;
+
+namespace IISWebManager.Application.Extensions
+{
+    public static class CommandExtensions
+    {
+        public static void ThrowIfNull(this ICommand command, string callerName)
+        {
+            if (command is null) throw new MissingCommandException($"Handler '{callerName}' received null query.");
+        }
+    }
+}
