b27769d890e71984396d90ba6cb6f638414f42b1
gixerr <pwardyn213@gmail.com>
2020-03-13 15:25:06 +0100

    added Exceptions

---
+++ src/IISWebManager.Application/Exceptions/InvalidIdentityTypeException.cs
@@ -0,0 +1,11 @@
+﻿namespace IISWebManager.Application.Exceptions
+{
+    public class InvalidIdentityTypeException : ApplicationException
+    {
+        public override string Code => "invalid_identity_type";
+        public InvalidIdentityTypeException(string identityType) 
+            : base($"Identity type '{identityType}' is invalid.")
+        {
+        }
+    }
+}
---
+++ src/IISWebManager.Application/Exceptions/MissingCommandException.cs
@@ -0,0 +1,11 @@
+﻿namespace IISWebManager.Application.Exceptions
+{
+    public class MissingCommandException : ApplicationException
+    {
+        public override string Code => "missing_command";
+        
+        public MissingCommandException(string message) : base(message)
+        {
+        }
+    }
+}
