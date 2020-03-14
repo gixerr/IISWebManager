2d29938c4d7aee3fb655d5de76e651e3bb1d746c
gixerr <pwardyn213@gmail.com>
2020-03-14 19:53:45 +0100

    added SiteExtensions

---
+++ src/IISWebManager.Infrastructure/Extensions/SiteExtensions.cs
@@ -0,0 +1,13 @@
+﻿using IISWebManager.Application.Exceptions;
+using Microsoft.Web.Administration;
+
+namespace IISWebManager.Infrastructure.Extensions
+{
+    public static class SiteExtensions
+    {
+        public static void ThrowIfNull(this Site site, string siteName)
+        {
+            if(site is null) throw new SiteNotExistsException(siteName);
+        }
+    }
+}
