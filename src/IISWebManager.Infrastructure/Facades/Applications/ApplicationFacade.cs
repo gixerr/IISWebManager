de0f98a03a6a6cc8d2ee08383398e0f8944dce74
gixerr <pwardyn213@gmail.com>
2020-03-14 20:00:49 +0100

    added ApplicationFacade

---
+++ src/IISWebManager.Infrastructure/Facades/Applications/ApplicationFacade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IISWebManager.Infrastructure.Extensions;
+using Microsoft.Web.Administration;
+ using App = Microsoft.Web.Administration.Application;
+ 
+ namespace IISWebManager.Infrastructure.Facades.Applications
+ {
+     public class ApplicationFacade : IApplicationFacade
+     {
+         private readonly ServerManager _serverManager;
+
+         public ApplicationFacade(ServerManager serverManager)
+         {
+             _serverManager = serverManager;
+         }
+         public IEnumerable<App> BrowseApplications() 
+             => _serverManager.Sites.SelectMany(x => x.Applications);
+ 
+         public ApplicationCollection GetSiteApplications(string siteName)
+         {
+             var site = _serverManager.Sites
+                 .SingleOrDefault(x => x.Name.Equals(siteName, StringComparison.OrdinalIgnoreCase));
+             site.ThrowIfNull(siteName);
+
+             return site.Applications;
+         }
+     }
+ }
---
+++ src/IISWebManager.Infrastructure/Facades/Applications/IApplicationFacade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.Web.Administration;
+using App = Microsoft.Web.Administration.Application;
+
+namespace IISWebManager.Infrastructure.Facades.Applications
+{
+    public interface IApplicationFacade
+    {
+        IEnumerable<App> BrowseApplications();
+        ApplicationCollection GetSiteApplications(string siteName);
+    }
+}
