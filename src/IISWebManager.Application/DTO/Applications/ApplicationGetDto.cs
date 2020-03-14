e731045d35cf57569c8a6414ece78104e6bdcbd2
gixerr <pwardyn213@gmail.com>
2020-03-14 20:21:18 +0100

    added ApplicationGetDto

---
+++ src/IISWebManager.Application/DTO/Applications/ApplicationGetDto.cs
@@ -0,0 +1,12 @@
+﻿using IISWebManager.Core.Domain;
+
+namespace IISWebManager.Application.DTO.Applications
+{
+    public class ApplicationGetDto
+    {
+        public string Name { get; set; }
+        public string ApplicationPoolName { get; set; }
+        public string ApplicationPoolStatus { get; set; }
+        public string PhysicalPath { get; set; }
+    }
+}
