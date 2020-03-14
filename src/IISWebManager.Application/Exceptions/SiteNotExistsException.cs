207d49ddf85e72bc1b36d93a8fa13cfa37646a3f
gixerr <pwardyn213@gmail.com>
2020-03-14 19:55:02 +0100

    added SiteNotExistsException

---
+++ src/IISWebManager.Application/Exceptions/SiteNotExistsException.cs
@@ -0,0 +1,11 @@
+﻿namespace IISWebManager.Application.Exceptions
+{
+    public class SiteNotExistsException : ApplicationException
+    {
+        public override string Code => "site_not_exists";
+        
+        public SiteNotExistsException(string message) : base(message)
+        {
+        }
+    }
+}
