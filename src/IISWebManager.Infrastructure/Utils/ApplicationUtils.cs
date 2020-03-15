fab028198a9b5fecafd42324ac68aaf219558f68
gixerr <pwardyn213@gmail.com>
2020-03-15 19:42:30 +0100

    added ApplicationUtils

---
+++ src/IISWebManager.Infrastructure/Utils/ApplicationUtils.cs
@@ -0,0 +1,11 @@
+﻿namespace IISWebManager.Infrastructure.Utils
+{
+    public static class ApplicationUtils
+    {
+        public static string ConvertPathToName(string path)
+            => path.Replace("/", string.Empty);
+
+        public static string ConvertNameToPath(string name)
+            => $"/{name}";
+    }
+}
