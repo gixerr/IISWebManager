036678d2e899174708c6e54fad36d4341b1048b1
gixerr <pwardyn213@gmail.com>
2020-03-15 23:03:49 +0100

    added ApplicationNotExistsException

---
+++ src/IISWebManager.Application/Exceptions/ApplicationNotExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace IISWebManager.Application.Exceptions
+{
+    public class ApplicationNotExistsException : ApplicationException
+    {
+        public override string Code => "application_not_exists";
+        
+        public ApplicationNotExistsException(string name) 
+            : base($"Application with name '{name}' doesn't exists.")
+        {
+        }
+    }
+}
