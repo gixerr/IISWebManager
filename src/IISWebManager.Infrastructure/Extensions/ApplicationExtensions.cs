6055dfd8cfd99ef496e97c8fccce5c0fb57e0313
gixerr <pwardyn213@gmail.com>
2020-03-15 23:03:29 +0100

    added ApplicationExtensions

---
+++ src/IISWebManager.Infrastructure/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using IISWebManager.Application.Exceptions;
+using App = Microsoft.Web.Administration.Application;
+
+namespace IISWebManager.Infrastructure.Extensions
+{
+    public static class ApplicationExtensions 
+    {
+        public static void ThrowIfNull(this App application, string name)
+        {
+            if (application is null) throw new ApplicationNotExistsException(name);
+        }
+    }
+}
