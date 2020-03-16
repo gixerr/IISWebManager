dd51165c34d8c30b5bbbfc38dd26159287d713ad
gixerr <pwardyn213@gmail.com>
2020-03-16 23:13:44 +0100

    refactored GetSiteApplicationsHandler

--- src/IISWebManager.Infrastructure/Handlers/Query/Applications/GetSiteApplicationsHandler.cs
+++ src/IISWebManager.Infrastructure/Handlers/Query/Applications/GetSiteApplicationsHandler.cs
@@ -4,17 +4,21 @@ using AutoMapper;
 using IISWebManager.Application.DTO.Applications;
 using IISWebManager.Application.Extensions;
 using IISWebManager.Application.Queries.Applications;
+using IISWebManager.Infrastructure.Extensions;
 using IISWebManager.Infrastructure.Facades.Applications;
+using IISWebManager.Infrastructure.Facades.Sites;
 
 namespace IISWebManager.Infrastructure.Handlers.Query.Applications
 {
     public class GetSiteApplicationsHandler : IQueryHandler<GetSiteApplications, IEnumerable<ApplicationGetDto>>
     {
+        private readonly ISiteFacade _siteFacade;
         private readonly IApplicationFacade _applicationFacade;
         private readonly IMapper _mapper;
 
-        public GetSiteApplicationsHandler(IApplicationFacade applicationFacade, IMapper mapper)
+        public GetSiteApplicationsHandler(ISiteFacade siteFacade, IApplicationFacade applicationFacade, IMapper mapper)
         {
+            _siteFacade = siteFacade;
             _applicationFacade = applicationFacade;
             _mapper = mapper;
         }
@@ -22,7 +26,9 @@ namespace IISWebManager.Infrastructure.Handlers.Query.Applications
         public IEnumerable<ApplicationGetDto> Handle(GetSiteApplications query)
         {
             query.ThrowIfNull(GetType().Name);
-            var applications = _applicationFacade.GetSiteApplications(query.Name);
+            var site = _siteFacade.GetSite(query.Name);
+            site.ThrowIfNull(query.Name);
+            var applications = _applicationFacade.GetSiteApplications(site);
             var applicationsDto = applications.Select(_mapper.Map<ApplicationGetDto>);
 
             return applicationsDto.OrderBy(x => x.Name);
