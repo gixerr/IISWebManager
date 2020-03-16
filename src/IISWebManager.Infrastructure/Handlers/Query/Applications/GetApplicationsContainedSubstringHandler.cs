dee865b2dbff2fef115433a09994f69632a7b32f
gixerr <pwardyn213@gmail.com>
2020-03-16 23:09:58 +0100

    refactored GetApplicationsContainedSubstringHandler

--- src/IISWebManager.Infrastructure/Handlers/Query/Applications/GetApplicationsContainedSubstringHandler.cs
+++ src/IISWebManager.Infrastructure/Handlers/Query/Applications/GetApplicationsContainedSubstringHandler.cs
@@ -3,8 +3,10 @@ using AutoMapper;
 using IISWebManager.Application.DTO.Applications;
 using IISWebManager.Application.Extensions;
 using IISWebManager.Application.Queries.Applications;
+using IISWebManager.Infrastructure.Extensions;
 using IISWebManager.Infrastructure.Facades.Applications;
 using IISWebManager.Infrastructure.Facades.Sites;
+using App = Microsoft.Web.Administration.Application;
 
 namespace IISWebManager.Infrastructure.Handlers.Query.Applications
 {
@@ -24,14 +26,25 @@ namespace IISWebManager.Infrastructure.Handlers.Query.Applications
         public IEnumerable<ApplicationGetDto> Handle(GetApplicationsContainedSubstring query)
         {
             query.ThrowIfNull(GetType().Name);
-            
-            
+            var applications = IsEmpty(query.SiteName)
+                ? _applicationFacade.GetApplications(query.SubString)
+                : GetSiteApplications(query.SubString, query.SiteName);
+   
             var applicationsDto = _mapper.Map<IEnumerable<ApplicationGetDto>>(applications);
 
             return applicationsDto;
         }
 
-        private bool IsEmpty(string value)
+        private static bool IsEmpty(string value)
             => string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value);
+
+        private IEnumerable<App> GetSiteApplications(string subString, string siteName)
+        {
+            var site = _siteFacade.GetSite(siteName);
+            site.ThrowIfNull(siteName);
+            var applications = _applicationFacade.GetSiteApplications(subString, site);
+
+            return applications;
+        }
     }
 }
