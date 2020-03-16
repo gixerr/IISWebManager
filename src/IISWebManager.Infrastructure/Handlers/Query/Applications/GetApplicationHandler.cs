41f881d521c7a4df26f51d0b34ff6c04dc8cbb8d
gixerr <pwardyn213@gmail.com>
2020-03-16 22:02:49 +0100

    refactored GetApplicationHandler

--- src/IISWebManager.Infrastructure/Handlers/Query/Applications/GetApplicationHandler.cs
+++ src/IISWebManager.Infrastructure/Handlers/Query/Applications/GetApplicationHandler.cs
@@ -4,23 +4,28 @@ using IISWebManager.Application.Extensions;
 using IISWebManager.Application.Queries.Applications;
 using IISWebManager.Infrastructure.Extensions;
 using IISWebManager.Infrastructure.Facades.Applications;
+using IISWebManager.Infrastructure.Facades.Sites;
 
 namespace IISWebManager.Infrastructure.Handlers.Query.Applications
 {
     public class GetApplicationHandler : IQueryHandler<GetApplication, ApplicationGetDto>
     {
+        private readonly ISiteFacade _siteFacade;
         private readonly IApplicationFacade _applicationFacade;
         private readonly IMapper _mapper;
 
-        public GetApplicationHandler(IApplicationFacade applicationFacade, IMapper mapper)
+        public GetApplicationHandler(ISiteFacade siteFacade, IApplicationFacade applicationFacade, IMapper mapper)
         {
+            _siteFacade = siteFacade;
             _applicationFacade = applicationFacade;
             _mapper = mapper;
         }
         public ApplicationGetDto Handle(GetApplication query)
         {
             query.ThrowIfNull(GetType().Name);
-            var application = _applicationFacade.GetApplication(query.Name, query.SiteName);
+            var site = _siteFacade.GetSite(query.SiteName);
+            site.ThrowIfNull(query.SiteName);
+            var application = _applicationFacade.GetApplication(query.Name, site);
             application.ThrowIfNull(query.Name);
             var applicationDto = _mapper.Map<ApplicationGetDto>(application);
 
