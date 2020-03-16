54759875ed5ee39790f93d54e9eb8f7f6b8818b3
gixerr <pwardyn213@gmail.com>
2020-03-16 21:59:05 +0100

    added SiteFacade

---
+++ src/IISWebManager.Infrastructure/Facades/Sites/ISiteFacade.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Web.Administration;
+
+namespace IISWebManager.Infrastructure.Facades.Sites
+{
+    public interface ISiteFacade
+    {
+        Site GetSite(string name);
+    }
+}
---
+++ src/IISWebManager.Infrastructure/Facades/Sites/SiteFacade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Web.Administration;
+
+namespace IISWebManager.Infrastructure.Facades.Sites
+{
+    public class SiteFacade : ISiteFacade
+    {
+        private readonly ServerManager _serverManager;
+
+        public SiteFacade(ServerManager serverManager)
+        {
+            _serverManager = serverManager;
+        }
+
+        public Site GetSite(string name) 
+            => _serverManager.Sites.SingleOrDefault(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+    }
+}
