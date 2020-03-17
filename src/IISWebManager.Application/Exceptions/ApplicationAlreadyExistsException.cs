5d32d1d98bf43771677be31184fc34ba31fcd333
gixerr <pwardyn213@gmail.com>
2020-03-17 15:19:06 +0100

    added ApplicationAlreadyExistsException

---
+++ src/IISWebManager.Application/Exceptions/ApplicationAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿namespace IISWebManager.Application.Exceptions
+{
+    public class ApplicationAlreadyExistsException : ApplicationException
+    {
+        public override string Code => "application_already_exists";
+        
+        public ApplicationAlreadyExistsException(string applicationName) 
+            : base($"Application with name '{applicationName}' already exists.")
+        {
+        }
+    }
+}
