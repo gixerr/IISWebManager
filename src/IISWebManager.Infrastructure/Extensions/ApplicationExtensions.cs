711b855ea768a0cd1e093bbeff3d2ff4b33e57f1
gixerr <pwardyn213@gmail.com>
2020-03-17 15:18:36 +0100

    added ThrowIfExists to ApplicationExtensions

--- src/IISWebManager.Infrastructure/Extensions/ApplicationExtensions.cs
+++ src/IISWebManager.Infrastructure/Extensions/ApplicationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using IISWebManager.Application.Exceptions;
+using IISWebManager.Infrastructure.Utils;
 using App = Microsoft.Web.Administration.Application;
 
 namespace IISWebManager.Infrastructure.Extensions
@@ -8,6 +9,12 @@ namespace IISWebManager.Infrastructure.Extensions
         public static void ThrowIfNull(this App application, string name)
         {
             if (application is null) throw new ApplicationNotExistsException(name);
+        }
+
+        public static void ThrowIfExists(this App application)
+        {
+            if (application is {})
+                throw new ApplicationAlreadyExistsException(ApplicationUtils.ConvertPathToName(application.Path));
         }
     }
 }
