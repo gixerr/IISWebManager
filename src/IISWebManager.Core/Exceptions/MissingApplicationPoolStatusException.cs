8a86c73375e49aa7c7cbc18d26da6da8b79fcb8a
gixerr <pwardyn213@gmail.com>
2020-03-22 22:12:52 +0100

    added MissingApplicationPoolStatusException.cs

---
+++ src/IISWebManager.Core/Exceptions/MissingApplicationPoolStatusException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IISWebManager.Core.Exceptions
+{
+    public class MissingApplicationPoolStatusException : DomainException
+    {
+        public override string Code => "missing_application_pool_status";
+        
+        public MissingApplicationPoolStatusException() : base("Application pool status is missing.")
+        {
+        }
+    }
+}
