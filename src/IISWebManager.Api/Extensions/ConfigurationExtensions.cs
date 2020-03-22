bb79f696d3df4446a86c8977cd236ecee45775a2
gixerr <pwardyn213@gmail.com>
2020-03-22 22:16:39 +0100

    added ConfigurationExtensions

---
+++ src/IISWebManager.Api/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.Extensions.Configuration;
+
+namespace IISWebManager.Api.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static TSettings BindSettings<TSettings>(this IConfiguration configuration, string sectionName)
+            where TSettings : new()
+        {
+            var settings = new TSettings();
+            configuration.GetSection(sectionName).Bind(settings);
+
+            return settings;
+        }
+    }
+}
