741c32f3f30c976b41a4fcb21b00acf60004aa01
gixerr <pwardyn213@gmail.com>
2020-03-22 22:12:12 +0100

    added Core ValueObjects

---
+++ src/IISWebManager.Core/ValueObjects/BuildApplication.cs
@@ -0,0 +1,26 @@
+﻿using IISWebManager.Core.Contracts;
+using IISWebManager.Core.Domain;
+using IISWebManager.Core.Exceptions;
+
+namespace IISWebManager.Core.ValueObjects
+{
+    public class BuildApplication : Model, IApplication
+    {
+        public string Name { get; private set; }
+        public IApplicationPool ApplicationPool { get; private set; }
+
+        public BuildApplication(string name, IApplicationPool applicationPool)
+        {
+            SetName(name);
+            SetApplicationPool(applicationPool);
+        }
+
+        private void SetName(string value)
+            => Name = ValueIsEmpty(value) ? throw new MissingApplicationNameException() : value;
+
+        private void SetApplicationPool(IApplicationPool applicationPool)
+            => ApplicationPool = ValueIsEmpty(applicationPool)
+                ? throw new MissingApplicationPoolException()
+                : applicationPool;
+    }
+}
---
+++ src/IISWebManager.Core/ValueObjects/BuildApplicationPool.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+using IISWebManager.Core.Contracts;
+using IISWebManager.Core.Domain;
+using IISWebManager.Core.Exceptions;
+
+namespace IISWebManager.Core.ValueObjects
+{
+    public class BuildApplicationPool : Model, IApplicationPool
+    {
+        public string Name { get; private set; }
+        public string Status { get; private set; }
+
+        public BuildApplicationPool(string name, string status)
+        {
+            SetName(name);
+            SetStatus(status);
+        }
+        
+        private void SetName(string value)
+            => Name = ValueIsEmpty(value) ? throw new MissingApplicationPoolNameException() : value;
+
+        private void SetStatus(string value)
+            => Status = ValueIsEmpty(value) ? throw new MissingApplicationPoolStatusException() : value;
+    }
+    
+}
