e744464edbf2e828a935c0851c5574371d765739
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 18:12:06 +0200

    added StopApplicationPool properties description

--- src/IISWebManager.Application/Commands/ApplicationPools/StopApplicationPool.cs
+++ src/IISWebManager.Application/Commands/ApplicationPools/StopApplicationPool.cs
@@ -1,11 +1,22 @@
-﻿namespace IISWebManager.Application.Commands.ApplicationPools
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IISWebManager.Application.Commands.ApplicationPools
 {
+    /// <summary>
+    /// Application pool to stop
+    /// </summary>
     public class StopApplicationPool : ICommand
     {
+        /// <summary>
+        /// Name of the application pool
+        /// </summary>
+        [Required]
+        public string Name { get; }
+        
         public StopApplicationPool(string applicationPoolName)
         {
             Name = applicationPoolName;
         }
-        public string Name { get; }
+        
     }
 }
