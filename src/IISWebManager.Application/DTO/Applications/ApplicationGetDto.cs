f2b6af34f2b9819b04b0cf2adc4b28d5ec8aa875
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 20:17:08 +0200

    added ApplicationGetDto properties description
    Signed-off-by: gixerr <pawel.wardyn@fenergo.com>

--- src/IISWebManager.Application/DTO/Applications/ApplicationGetDto.cs
+++ src/IISWebManager.Application/DTO/Applications/ApplicationGetDto.cs
@@ -1,10 +1,28 @@
 ﻿namespace IISWebManager.Application.DTO.Applications
 {
+    /// <summary>
+    /// Application 'GET' object
+    /// </summary>
     public class ApplicationGetDto
     {
+        /// <summary>
+        /// Name of the application
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Name of the application pool
+        /// </summary>
         public string ApplicationPoolName { get; set; }
+        
+        /// <summary>
+        /// Status of the application pool
+        /// </summary>
         public string ApplicationPoolStatus { get; set; }
+        
+        /// <summary>
+        /// Physical path of the application
+        /// </summary>
         public string PhysicalPath { get; set; }
     }
 }
