b5b5e03eb71f03435e65ef07fc51be8234a56dce
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 20:17:26 +0200

    added DeleteApplication properties description
    Signed-off-by: gixerr <pawel.wardyn@fenergo.com>

--- src/IISWebManager.Application/Commands/Applications/DeleteApplication.cs
+++ src/IISWebManager.Application/Commands/Applications/DeleteApplication.cs
@@ -1,8 +1,18 @@
 ﻿namespace IISWebManager.Application.Commands.Applications
 {
+    /// <summary>
+    /// Application to delete
+    /// </summary>
     public class DeleteApplication : ICommand
     {
+        /// <summary>
+        /// Site name
+        /// </summary>
         public string SiteName { get; set; }
+        
+        /// <summary>
+        /// Application name
+        /// </summary>
         public string ApplicationName { get; set; }
     }
 }
