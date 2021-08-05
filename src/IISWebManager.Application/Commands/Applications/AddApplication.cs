d8de48853f8c373b9678da4c66313eba1059bafb
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 20:16:51 +0200

    added AddApplication properties description
    Signed-off-by: gixerr <pawel.wardyn@fenergo.com>

--- src/IISWebManager.Application/Commands/Applications/AddApplication.cs
+++ src/IISWebManager.Application/Commands/Applications/AddApplication.cs
@@ -1,10 +1,34 @@
-﻿namespace IISWebManager.Application.Commands.Applications
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IISWebManager.Application.Commands.Applications
 {
+    /// <summary>
+    /// Application to add 
+    /// </summary>
     public class AddApplication : ICommand
     {
+        /// <summary>
+        /// Name of the application
+        /// </summary>
+        [Required]
         public string ApplicationName { get; set; }
+        
+        /// <summary>
+        /// Application physical path
+        /// </summary>
+        [Required]
         public string PhysicalPath { get; set; }
+        
+        /// <summary>
+        /// Application pool name
+        /// </summary>
+        [Required]
         public string ApplicationPoolName { get; set; }
+        
+        /// <summary>
+        /// Site name
+        /// </summary>
+        [Required]
         public string SiteName { get; set; }
     }
 }
