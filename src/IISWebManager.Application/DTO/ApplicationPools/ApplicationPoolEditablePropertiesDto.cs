f96b05ab2494c774b383405c61585d2060c5f999
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 18:08:17 +0200

    added ApplicationPoolEditablePropertiesDto properties description

--- src/IISWebManager.Application/DTO/ApplicationPools/ApplicationPoolEditablePropertiesDto.cs
+++ src/IISWebManager.Application/DTO/ApplicationPools/ApplicationPoolEditablePropertiesDto.cs
@@ -1,10 +1,28 @@
 ﻿namespace IISWebManager.Application.DTO.ApplicationPools
 {
+    /// <summary>
+    /// Application pool editable properties object
+    /// </summary>
     public class ApplicationPoolEditablePropertiesDto
     {
+        /// <summary>
+        /// Name of the application pool
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Managed pipeline mode of the application pool
+        /// </summary>
         public string ManagedPipelineMode { get; set; }
+        
+        /// <summary>
+        /// Managed run time version of the application pool
+        /// </summary>
         public string ManagedRuntimeVersion { get; set; }
+        
+        /// <summary>
+        /// Identity of the application pool 
+        /// </summary>
         public string Identity { get; set; }
     }
 }
