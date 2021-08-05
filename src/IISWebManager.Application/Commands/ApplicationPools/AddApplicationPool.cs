f926fa1f0a56af04392b60c50bfa4267a22046ed
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 18:07:50 +0200

    added AddApplicationPool properties description

--- src/IISWebManager.Application/Commands/ApplicationPools/AddApplicationPool.cs
+++ src/IISWebManager.Application/Commands/ApplicationPools/AddApplicationPool.cs
@@ -1,10 +1,34 @@
-﻿namespace IISWebManager.Application.Commands.ApplicationPools
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IISWebManager.Application.Commands.ApplicationPools
 {
+    /// <summary>
+    /// Application pool to add 
+    /// </summary>
     public class AddApplicationPool : ICommand
     {
+        /// <summary>
+        /// Name of the application pool
+        /// </summary>
+        [Required]
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Application pool managed pipeline mode
+        /// </summary>
+        [Required]
         public string ManagedPipelineMode { get; set; }
+        
+        /// <summary>
+        /// Application pool managed runtime version
+        /// </summary>
+        [Required]
         public string ManagedRuntimeVersion { get; set; }
+        
+        /// <summary>
+        /// Should application pool start automatically
+        /// </summary>
+        [Required]
         public bool AutoStart { get; set; } = true;
     }
 }
