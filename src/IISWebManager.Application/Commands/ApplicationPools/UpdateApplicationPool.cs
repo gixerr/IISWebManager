2629e05207183f6d6cd5fb4ac980f5f27c8ae561
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 18:12:18 +0200

    added UpdateApplicationPool properties description

--- src/IISWebManager.Application/Commands/ApplicationPools/UpdateApplicationPool.cs
+++ src/IISWebManager.Application/Commands/ApplicationPools/UpdateApplicationPool.cs
@@ -1,11 +1,33 @@
 ﻿namespace IISWebManager.Application.Commands.ApplicationPools
 {
+    /// <summary>
+    /// Application pool to update 
+    /// </summary>
     public class UpdateApplicationPool : ICommand
     {
+        /// <summary>
+        /// Name of the application pool
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// New name of the application pool
+        /// </summary>
         public string NewName { get; set; }
+        
+        /// <summary>
+        /// Application pool managed pipeline mode
+        /// </summary>
         public string ManagedPipelineMode { get; set; }
+        
+        /// <summary>
+        /// Application pool managed runtime version
+        /// </summary>
         public string ManagedRuntimeVersion { get; set; }
+        
+        /// <summary>
+        /// Application pool identity
+        /// </summary>
         public string Identity { get; set; }
     }
 }
