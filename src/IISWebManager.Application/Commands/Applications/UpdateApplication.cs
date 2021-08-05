509ad349aaec7ff9bf3a9d9967c3244d83019ecc
gixerr <pawel.wardyn@fenergo.com>
2021-08-05 20:17:45 +0200

    added UpdateApplicationPool properties description
    Signed-off-by: gixerr <pawel.wardyn@fenergo.com>

--- src/IISWebManager.Application/Commands/Applications/UpdateApplication.cs
+++ src/IISWebManager.Application/Commands/Applications/UpdateApplication.cs
@@ -1,11 +1,33 @@
 ﻿namespace IISWebManager.Application.Commands.Applications
 {
+    /// <summary>
+    /// Application to update 
+    /// </summary>
     public class UpdateApplication : ICommand
     {
+        /// <summary>
+        /// Site name
+        /// </summary>
         public string SiteName { get; set; }
+        
+        /// <summary>
+        /// Application to update name
+        /// </summary>
         public string ApplicationName { get; set; }
+        
+        /// <summary>
+        /// New application name
+        /// </summary>
         public string NewApplicationName { get; set; }
+        
+        /// <summary>
+        /// Application pool name
+        /// </summary>
         public string ApplicationPoolName { get; set; }
+        
+        /// <summary>
+        /// Application physical path
+        /// </summary>
         public string PhysicalPath { get; set; }
     }
 }
